e37d746149979625604204bd690fee469c61ffab
EXH3Y <EXH3Y@users.noreply.github.com>
2015-12-13 13:46:58 +0100

    Fixed tests

--- telebot/src/test/java/exh3y/telebot/data/TelegramMessageTest.java
+++ telebot/src/test/java/exh3y/telebot/data/TelegramMessageTest.java
@@ -13,7 +13,7 @@ public class TelegramMessageTest {
 		JSONObject json = new JSONObject("{\"message_id\":42,\"from\":{\"id\":547885,\"first_name\":\"Some\",\"last_name\":\"User\",\"username\":\"testuser\"},\"chat\":{\"id\":1337,\"first_name\":\"Some\",\"last_name\":\"User\",\"username\":\"testuser\",\"type\":\"private\"},\"date\":1450006107,\"text\":\"This is a test message.\"}");
 		TelegramMessage telemessage = new TelegramMessage(json);
 		
-		assertTrue(telemessage.getText() == "This is a test message.");
+		assertTrue(telemessage.getText().equals("This is a test message."));
 		assertTrue(telemessage.getChatId() == 1337);
 		
 	}
