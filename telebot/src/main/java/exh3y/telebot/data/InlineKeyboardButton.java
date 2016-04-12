0295562ca4e0b702b8291d438a6ad25d56119788
EXH3Y <EXH3Y@users.noreply.github.com>
2016-04-12 16:57:38 +0200

    Added InlineKeyboards as described in https://core.telegram.org/bots/api#inlinekeyboardmarkup

---
+++ telebot/src/main/java/exh3y/telebot/data/InlineKeyboardButton.java
@@ -0,0 +1,96 @@
+package exh3y.telebot.data;
+
+import org.json.JSONObject;
+import org.json.JSONString;
+
+public class InlineKeyboardButton implements JSONString {
+
+	private String	text;
+	private String	url;
+	private String	callbackData;
+	private String	switchInlineQuery;
+
+	/**
+	 * Creates a new button for use in an InlineKeyboardMarkup
+	 * 
+	 * @param text
+	 *            The text to show
+	 * @param url
+	 * @param callbackData
+	 * @param switchInlineQuery
+	 * @see <a href="https://core.telegram.org/bots/api#inlinekeyboardbutton">
+	 *      https://core.telegram.org/bots/api#inlinekeyboardbutton</a>
+	 */
+	public InlineKeyboardButton(String text, String url, String callbackData, String switchInlineQuery) {
+		this.setText(text);
+		this.setUrl(url);
+		this.setCallbackData(callbackData);
+		this.setSwitchInlineQuery(switchInlineQuery);
+	}
+
+	public InlineKeyboardButton(String text) {
+		this(text, null, null, null);
+	}
+
+	public String getText() {
+
+		return text;
+	}
+
+	public void setText(String text) {
+
+		this.text = text;
+	}
+
+	public String getUrl() {
+
+		return url;
+	}
+
+	public void setUrl(String url) {
+
+		this.url = url;
+	}
+
+	public String getCallbackData() {
+
+		return callbackData;
+	}
+
+	public void setCallbackData(String callbackData) {
+
+		this.callbackData = callbackData;
+	}
+
+	public String getSwitchInlineQuery() {
+
+		return switchInlineQuery;
+	}
+
+	public void setSwitchInlineQuery(String switchInlineQuery) {
+
+		this.switchInlineQuery = switchInlineQuery;
+	}
+
+	@Override
+	public String toJSONString() {
+
+		JSONObject object = new JSONObject();
+		object.put("text", text);
+
+		if (url != null) {
+			object.put("url", url);
+		}
+
+		if (callbackData != null) {
+			object.put("callback_data", callbackData);
+		}
+
+		if (switchInlineQuery != null) {
+			object.put("switch_inline_query", switchInlineQuery);
+		}
+
+		return object.toString();
+	}
+
+}
